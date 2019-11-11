51f43aee18155ea47268421e53edc0d5b2cea4b9
tslumley <t.lumley@auckland.ac.nz>
2019-11-12 09:05:59 +1300

    Now with ggplot

---
+++ R/geom_dhb.R
@@ -0,0 +1,25 @@
+geom_dhb<-function(mapping = NULL, data = NULL, ..., na.rm = FALSE, show.legend = NA, inherit.aes = TRUE){
+	map<-dhmap_hex()
+	list(
+            geom_map(mapping, data, ..., na.rm=na.rm, show.legend=show.legend, inherit.aes=inherit.aes,map=map),
+            expand_limits(map),
+            coord_fixed(),
+            theme(axis.title.x=element_blank(),
+                  axis.text.x=element_blank(), axis.title.y=element_blank(),
+                  axis.text.y=element_blank())
+	)
+	
+}
+
+
+geom_label_dhb<-function(mapping = NULL, data = NULL, ..., na.rm = FALSE, show.legend = NA, inherit.aes = TRUE,short=FALSE){
+	d<-data.frame(
+		x=dhbs$x,
+		y=dhbs$y,
+		dhb_labels=if(short) 
+					dhbs$shortname 
+				else 
+					dhbs$printname
+	)
+	geom_text(data=d, aes(x,y,label=dhb_labels),...)
+}
