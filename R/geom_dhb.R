f7e557e77b394174bd76a087a14f2b0cb25196b4
Thomas Lumley <t.lumley@auckland.ac.nz>
2019-11-19 14:45:51 +1300

    ggplot version now does radius

--- R/geom_dhb.R
+++ R/geom_dhb.R
@@ -1,4 +1,4 @@
-geom_dhb<-function(mapping = NULL, data = NULL, ..., na.rm = FALSE, show.legend = NA, inherit.aes = TRUE){
+geom_dhb_old<-function(mapping = NULL, data = NULL, ..., na.rm = FALSE, show.legend = NA, inherit.aes = TRUE){
 	map<-dhmap_hex()
 	list(
             geom_map(mapping, data, ..., na.rm=na.rm, show.legend=show.legend, inherit.aes=inherit.aes,map=map),
@@ -23,3 +23,96 @@ geom_label_dhb<-function(mapping = NULL, data = NULL, ..., na.rm = FALSE, show.l
 	)
 	geom_text(data=d, aes(x,y,label=dhb_labels),...)
 }
+
+
+geom_dhb <- function(mapping = NULL, data = NULL,
+                      stat = "identity", 
+                     ...,
+                     na.rm = FALSE,
+                     show.legend = NA,
+                     inherit.aes = TRUE,coord=TRUE) {
+
+
+
+    
+    rval<-list( 
+        layer(
+            data = data,
+            mapping = mapping,
+            stat = stat,
+            geom = GeomDHBmap,
+            position = PositionIdentity,
+            show.legend = show.legend,
+            inherit.aes = inherit.aes,
+            params = list(
+                na.rm = na.rm,
+                ...
+            )),
+
+        theme(axis.title.x=element_blank(),
+                  axis.text.x=element_blank(), axis.title.y=element_blank(),
+                  axis.text.y=element_blank(),panel.grid=element_blank())
+    )
+    if(coord){
+        rval<-c(rval,
+            coord_fixed(),
+            expand_limits(dhmap_hex()))
+        }
+     rval
+
+}
+
+
+"%||%" <- function (a, b) 
+{
+    if (!is.null(a)) 
+        a
+    else b
+}
+
+GeomDHBmap <- ggproto("GeomMap", GeomPolygon,
+                        default_aes = aes(colour = "NA", fill = "grey20", size = 0.5, linetype = 1,
+                                          alpha = NA, subgroup = NULL, radius=0.95),
+  draw_panel = function(data, panel_params, coord, map) {
+
+      hex_x <- hex_point
+      hex_y <- hex_flat
+      ## need to reorder sizes to match order
+      radius<-data$radius
+      if (max(radius)>1)
+          radius<-0.95*radius/max(radius)
+      
+      idx<-dhb_lookup(data$map_id)
+      radius<-radius[idx]
+	
+    map<-na.omit(
+        data.frame(
+            x=as.vector(t(outer(radius, hex_x) + dhbs$x)),
+            y= as.vector(t(outer(radius, hex_y) + dhbs$y)),
+            id =rep(dhbs$keyname,each=8)
+        )
+    )
+    common <- intersect(data$map_id, map$id)
+    data <- data[data$map_id %in% common, , drop = FALSE]
+    map <- map[map$id %in% common, , drop = FALSE]
+
+    # Munch, then set up id variable for polygonGrob -
+    # must be sequential integers
+    coords <- coord_munch(coord, map, panel_params)
+    coords$group <- coords$group %||% coords$id
+    grob_id <- match(coords$group, unique(coords$group))
+
+    # Align data with map
+    data_rows <- match(coords$id[!duplicated(grob_id)], data$map_id)
+    data <- data[data_rows, , drop = FALSE]
+
+    grid::polygonGrob(coords$x, coords$y, default.units = "native", id = grob_id,
+      gp = grid::gpar(
+        col = data$colour, fill = alpha(data$fill, data$alpha),
+        lwd = data$size * .pt
+      )
+    )
+  },
+
+  required_aes = c("map_id")
+)
